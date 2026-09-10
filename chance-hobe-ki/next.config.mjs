0cc0609045c552595a775faa39fe8a8f9721d569
Nishat Alam <alamnishat456@gmail.com>
2026-09-10 08:26:59 +0600

    Add files via upload

--- chance-hobe-ki/next.config.mjs
+++ chance-hobe-ki/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "commons.wikimedia.org" },
+      { protocol: "https", hostname: "thumb.wikimedia.org" },
+      { protocol: "https", hostname: "wallpaperaccess.com" },
+      { protocol: "https", hostname: "1.bp.blogspot.com" },
+      { protocol: "https", hostname: "bdtalika.com" },
+      { protocol: "https", hostname: "ecdn.dhakatribune.net" },
+      { protocol: "https", hostname: "files.thedailycampus.com" },
+      { protocol: "https", hostname: "just.edu.bd" },
+      { protocol: "https", hostname: "ku.ac.bd" },
+      { protocol: "https", hostname: "www.hstu.ac.bd" },
+    ],
+  },
+};
 
 export default nextConfig;
