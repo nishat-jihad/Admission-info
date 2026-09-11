df861b8d7a5bbb7b1c5a139b445e4e6fb903aeae
Nishat Alam <alamnishat456@gmail.com>
2026-09-11 23:14:13 +0600

    Update layout.js

--- chance-hobe-ki/src/app/layout.js
+++ chance-hobe-ki/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script";
 import { Bangers, Hind_Siliguri, Baloo_Da_2 } from "next/font/google";
 import "./globals.css";
 
@@ -80,6 +81,15 @@ export default function RootLayout({ children }) {
       <body className={`${bangers.variable} ${hindSiliguri.variable} ${balooDa2.variable}`}>
         {children}
         <Analytics />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a] = c[a] || function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ygp6jepq9e");
+          `}
+        </Script>
       </body>
     </html>
   );
